7d9d20c009520ffa1263a67c12c94dae09a42e33
Sunrise <dragonzhengyew@gmail.com>
2021-06-04 23:25:56 +0800

    reads from sector 2

--- bootloader/boot.asm
+++ bootloader/boot.asm
@@ -19,9 +19,27 @@ start:
     mov ss, ax
     mov sp, 0x7c00
     sti
-    mov si, message
+
+    mov si, initMessage
+    call print
+
+; SET UP FOR INT 0x13 TO READ SECTOR 2 FROM DISK
+    mov ah, 2
+    mov al, 1
+    mov ch, 0
+    mov cl, 2
+    mov dh, 0
+    mov bx, buffer
+    int 0x13
+    jc error
+    mov si, buffer
     call print
     jmp $           ; jump to self to prevent executing signature
+
+error:
+    mov si, errorMessage
+    call print
+    jmp $
 
 print:
     mov ah, 0eh     ; print
@@ -36,7 +54,10 @@ printChar:
     int 0x10        ; print interrupt
     ret
 
-message: db 'Hello, world!', 0
+initMessage: db 'Hello, world!', 0xD, 0
+errorMessage: db "An error has occured.", 0xD, 0
 
 times 510-($ - $$) db 0
 dw 0xAA55
+
+buffer:
